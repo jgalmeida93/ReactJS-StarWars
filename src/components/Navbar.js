11db4182dbe22c1e1895169114c383500f08c00b
Jonas Gabriel <jgalmeida1993@gmail.com>
2020-03-09 21:58:49 -0300

    :construction: Cria Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,23 @@
 import React from "react";
+import { Menu, Container } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
-  return <div></div>;
+  return (
+    <div>
+      <Menu inverted color="black">
+        <Container>
+          <Link to="/">
+            <Menu.Item name="Star wars API"></Menu.Item>
+          </Link>
+          <Link to="/personagens">
+            <Menu.Item name="Personagens"></Menu.Item>
+          </Link>
+          <Link to="/planetas">
+            <Menu.Item name="Planetas"></Menu.Item>
+          </Link>
+        </Container>
+      </Menu>
+    </div>
+  );
 }

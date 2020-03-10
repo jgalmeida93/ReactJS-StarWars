425e2142b73bca06e1dba47a2919ba81bcfc399b
Jonas Gabriel <jgalmeida1993@gmail.com>
2020-03-10 17:59:12 -0300

    :lipstick: Adiciona class home

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Home() {
   return (
-    <div>
+    <div className="home">
       <h1 className="header-white">Seja Bem vindo a STAR WARS Api</h1>
     </div>
   );

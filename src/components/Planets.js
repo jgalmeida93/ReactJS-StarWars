a8a5805e33bb9c639653524d8f57af8843d52591
Jonas Gabriel <jgalmeida1993@gmail.com>
2020-03-09 21:59:14 -0300

    :construction: Cria Planet Component

---
+++ src/components/Planets.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Card, Grid } from "semantic-ui-react";
+
+export default function Planets({ data }) {
+  // data que é passada pelo App é recebida aqui
+  return (
+    <>
+      <h1>Planetas</h1>
+      <Grid columns={3}>
+        {data.map((planets, i) => {
+          return (
+            <Grid.Column key={i}>
+              <Card>
+                <Card.Content>
+                  <Card.Header>{planets.name}</Card.Header>
+                  <Card.Description>
+                    <strong>Clima</strong>
+                    <p>{planets.climate}</p>
+                    <strong>Diâmetro</strong>
+                    <p>{planets.diameter}</p>
+                    <strong>População</strong>
+                    <p>{planets.population}</p>
+                  </Card.Description>
+                </Card.Content>
+              </Card>
+            </Grid.Column>
+          );
+        })}
+      </Grid>
+    </>
+  );
+}

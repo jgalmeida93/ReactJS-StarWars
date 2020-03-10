92bf77d3e0bfd58424ae503ecf5baba7c814e04f
Jonas Gabriel <jgalmeida1993@gmail.com>
2020-03-09 22:44:02 -0300

    :lipstick: Altera header da home

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Home() {
   return (
     <div>
-      <p>Home</p>
+      <h1 className="header-white">Seja Bem vindo a STAR WARS Api</h1>
     </div>
   );
 }
